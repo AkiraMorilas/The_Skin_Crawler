60aa686f9d26e5e1a09731c8971780879e0bed60
Akira Morilas <morilasakira@tutanota.com>
2023-05-30 19:44:53 +0200

    Add definitions

---
+++ skincrawler.py
@@ -0,0 +1,29 @@
+# keyboard definitions
+k1 = [thumb]
+k2 = [index,index]
+k3 = [index,thumb,index]
+k4 = [middle,index,index,middle]
+k5 = [middle,index,thumb,index,middle]
+k6 = [ring,middle,index,index,middle,ring]
+k7 = [ring,middle,index,thumb,index,middle,ring]
+k8 = [ring,middle,index,thumb,thumb,index,middle,ring]
+k9 = [pinky,ring,middle,index,thumb,index,middle,ring,pinky]
+k10 = [pinky,ring,middle,index,thumb,thumb,index,middle,ring,pinky]
+k18 = [pinky,ring,middle,index,pinky,ring,middle,index,thumb,thumb,index,middle,ring,pinky,index,middle,ring,pinky]
+kkeys = [k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k18]
+# beatmania definitions
+b5 = [scratch,white,blue,white,blue,white]
+b7 = [scratch,white,blue,white,blue,white,blue,white]
+b10 = [scratch,white,blue,white,blue,white,white,blue,white,blue,white,scratch]
+b14 = [scratch,white,blue,white,blue,white,blue,white,white,blue,white,blue,white,blue,white,scratch]
+bkeys = [b5,b7,b10,b14]
+# ez2dj definitions
+e5 = [scratch,white,blue,white,blue,white,foot]
+e7 = [scratch,white,blue,white,blue,white,foot,red,red]
+e10 = [scratch,white,blue,white,blue,white,foot,white,blue,white,blue,white,scratch]
+e14 = [scratch,white,blue,white,blue,white,red,red,red,red,white,blue,white,blue,white,scratch]
+e16 = [scratch,white,blue,white,blue,white,foot,red,red,red,red,foot,white,blue,white,blue,white,scratch]
+ekeys = [e5,e7,e10,e14,e16]
+# insert other definitions here maybe idk
+
+
